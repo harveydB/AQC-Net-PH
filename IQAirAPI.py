777d3a2fee659ca03657a32d69b311dbd3b15997
Kaldr4 <80321695+Kaldr4@users.noreply.github.com>
2022-03-19 16:40:08 +0800

    Add files via upload

---
+++ IQAirAPI.py
@@ -0,0 +1,18 @@
+import requests
+import json
+
+URL = "http://api.airvisual.com/v2"
+
+City = 'Caloocan'
+State = 'NCR'
+Country = 'Philippines'
+APIKEY = '75735f79-9de0-4104-a595-e555e240f4c9'
+
+queryURL = URL + '/city?city=' + City + '&state=' + State + '&country=' + Country + '&key=' +APIKEY
+
+def getAQI():
+    response = requests.get(queryURL)
+    userdata = json.loads(response.text)
+    return userdata.get('data').get('current').get('pollution').get('aqius')
+
+print(getAQI())
