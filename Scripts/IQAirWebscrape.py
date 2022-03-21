483823349eaed49d71b6e5df50911401d657d4c2
Kaldr4 <80321695+Kaldr4@users.noreply.github.com>
2022-03-21 14:42:10 +0800

    Update IQAirWebscrape.py

--- Scripts/IQAirWebscrape.py
+++ Scripts/IQAirWebscrape.py
@@ -8,6 +8,6 @@ page = requests.get(queryURL)
 soup = BeautifulSoup(page.text, "lxml")
 results = soup.find(class_="aqi-overview-detail__other-pollution-table")
 textdata = results.text
-textdata = textdata.split()
-pm_value = float(textdata[2])
+pm_value = float(textdata.split()[2])
 print(pm_value)
+
