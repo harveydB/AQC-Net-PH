b21a2b785d0d3288b9d7b1b51a3d52bae6be377c
Kaldr4 <80321695+Kaldr4@users.noreply.github.com>
2022-03-31 17:39:54 +0800

    Update IQAirWebscrape.py

--- Scripts/IQAirWebscrape.py
+++ Scripts/IQAirWebscrape.py
@@ -2,12 +2,30 @@ from cgitb import text
 import requests
 from bs4 import BeautifulSoup
 import pandas as pd
+import time
+import re 
+import csv
 
-queryURL = 'https://www.iqair.com/philippines/ncr/caloocan'
-page = requests.get(queryURL)
-soup = BeautifulSoup(page.text, "lxml")
-results = soup.find(class_="aqi-overview-detail__other-pollution-table")
-textdata = results.text
-pm_value = float(textdata.split()[2])
-print(pm_value)
+pm_array = []
+ctr = 0
+
+while ctr<17:
+    queryURL = 'https://www.iqair.com/philippines/ncr/caloocan'
+    page = requests.get(queryURL)
+    soup = BeautifulSoup(page.text, "lxml")
+    results = soup.find(class_="aqi-overview-detail__other-pollution-table")
+    textdata = str(results.text)
+    textdata = re.sub('[^a-zA-Z0-9 \n\.]', ' ', textdata)
+    pm_value = float(textdata.split()[2])
+    pm_array.append(pm_value)
+    time.sleep(1)                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                           
+    ctr+=1
+
+columns = ['IQAir PM Value'] 
+
+with open('C:/Users/Kaldra/Desktop/School/A2ndSem21-22/CoE 199/Datasets/IQAIR Api.csv', 'w', encoding='UTF8', newline='') as f:
+    writer = csv.writer(f)
+    writer.writerow(columns)
+    for val in pm_array:
+        writer.writerow([val])
 
