74a08c4120c84fc3243dedd08180a5c5ec2dbe54
jervinjosh68 <jervin.josh.valencia@eee.upd.edu.ph>
2022-04-02 13:32:44 +0800

    Update IQAirWebscrape.py

--- Scripts/IQAirWebscrape.py
+++ Scripts/IQAirWebscrape.py
@@ -6,12 +6,11 @@ import time
 import re 
 import csv
 
-
-def get_PM(queryURL):
+def getAQI(city,filename):
     pm_array = []
     ctr = 0
-
-    while ctr<17: 
+    queryURL = 'https://www.iqair.com/philippines/ncr/' + city
+    while ctr<17:
         page = requests.get(queryURL)
         soup = BeautifulSoup(page.text, "lxml")
         results = soup.find(class_="aqi-overview-detail__other-pollution-table")
@@ -19,19 +18,14 @@ def get_PM(queryURL):
         textdata = re.sub('[^a-zA-Z0-9 \n\.]', ' ', textdata)
         pm_value = float(textdata.split()[2])
         pm_array.append(pm_value)
-        time.sleep(1800)                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                           
+        time.sleep(1)                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                           
         ctr+=1
-    return pm_array
 
-def write_PM(filename,pm_array):
     columns = ['IQAir PM Value'] 
-
-    with open(filename, 'w', encoding='UTF8', newline='') as f:
+    with open('C:/Users/Kaldra/Desktop/School/A2ndSem21-22/CoE 199/Datasets/'+ filename, 'w', encoding='UTF8', newline='') as f:
         writer = csv.writer(f)
         writer.writerow(columns)
         for val in pm_array:
             writer.writerow([val])
 
-queryURL = 'https://www.iqair.com/philippines/ncr/caloocan'
-pm_array = get_PM(queryURL)
-write_PM("D:/GitHub/EEE-199/PM_data/04-02-2022_Caloocan_IQAir",pm_array)
+#getAQI('caloocan', 'caloocan.csv' )
